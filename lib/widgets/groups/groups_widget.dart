fe63baf608a41c95e075b431d847b95a6ab31556
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-12 23:45:14 +0300

    other code + flutter_slidable

--- lib/widgets/groups/groups_widget.dart
+++ lib/widgets/groups/groups_widget.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
 
-class GroupsWidget extends StatefulWidget {
+class GroupsWidget extends StatelessWidget {
   const GroupsWidget({super.key});
 
   @override
-  State<GroupsWidget> createState() => _GroupsWidgetState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          "Группы",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: const _GroupListWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
 
-class _GroupsWidgetState extends State<GroupsWidget> {
+class _GroupListWidget extends StatefulWidget {
+  const _GroupListWidget();
+
+  @override
+  State<_GroupListWidget> createState() => _GroupListWidgetState();
+}
+
+class _GroupListWidgetState extends State<_GroupListWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.separated(
+      itemCount: 100,
+      separatorBuilder: (BuildContext context, int index) {
+        return _GroupListRowWidget(indexInList: index);
+      },
+      itemBuilder: (BuildContext context, int index) {
+        return const Divider(height: 3);
+      },
+    );
+  }
+}
+
+class _GroupListRowWidget extends StatelessWidget {
+  final int indexInList;
+  const _GroupListRowWidget({required this.indexInList});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ListTile(
+      title: Text("sdjvnsdjk vnvjsnvj vehvnjvnsj"),
+      trailing: Icon(Icons.chevron_right),
+    );
   }
 }
