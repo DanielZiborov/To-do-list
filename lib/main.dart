c32bc2d2eea3659ffef31710ddf678250e428e1b
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-12 17:42:58 +0300

    main fixe

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/widgets/app/my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(),
-    );
-  }
 }
