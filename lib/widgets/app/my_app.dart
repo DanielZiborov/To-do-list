7b2e8c1680179da2a43be5b1970300333935880d
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-12 17:42:21 +0300

    groups_widget

---
+++ lib/widgets/app/my_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/widgets/groups/groups_widget.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/groups/':(context) => const GroupsWidget(),
+      },
+    );
+  }
+}
