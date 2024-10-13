7c3f0a551b751a2b93f3853e599357e2b04d0538
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-13 14:02:35 +0300

    add text field

--- lib/widgets/group_form/group_form_widget.dart
+++ lib/widgets/group_form/group_form_widget.dart
@@ -11,6 +11,29 @@ class GroupFormWidget extends StatelessWidget {
         title: const Text("Новая группа"),
         centerTitle: true,
       ),
+      body: Center(
+        child: Container(
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: _GroupNameWidget(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _GroupNameWidget extends StatelessWidget {
+  const _GroupNameWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+      autofocus: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'Имя группы',
+      ),
     );
   }
 }
