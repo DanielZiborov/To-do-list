3d153017823ee425bc40b8b67f78c86491cf456d
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-13 20:52:45 +0300

    print(groupName)

--- lib/widgets/group_form/group_form_widget.dart
+++ lib/widgets/group_form/group_form_widget.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/widgets/group_form/group_form_widget_model.dart';
 
-class GroupFormWidget extends StatelessWidget {
+class GroupFormWidget extends StatefulWidget {
   const GroupFormWidget({super.key});
 
+  @override
+  State<GroupFormWidget> createState() => _GroupFormWidgetState();
+}
+
+class _GroupFormWidgetState extends State<GroupFormWidget> {
+  final _model = GroupFormWidgetModel();
+  @override
+  Widget build(BuildContext context) {
+    return GroupFormWidgetModelProvider(
+      model: _model,
+      child: const GroupFormWidgetBody(),
+    );
+  }
+}
+
+class GroupFormWidgetBody extends StatelessWidget {
+  const GroupFormWidgetBody({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +36,9 @@ class GroupFormWidget extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => GroupFormWidgetModelProvider.read(context)
+            ?.model
+            .saveGroup(context),
         child: const Icon(Icons.done),
       ),
     );
@@ -27,15 +47,17 @@ class GroupFormWidget extends StatelessWidget {
 
 class _GroupNameWidget extends StatelessWidget {
   const _GroupNameWidget();
-
   @override
   Widget build(BuildContext context) {
-    return const TextField(
+    final model = GroupFormWidgetModelProvider.read(context)?.model;
+    return TextField(
       autofocus: true,
-      decoration: InputDecoration(
+      decoration: const InputDecoration(
         border: OutlineInputBorder(),
         hintText: 'Имя группы',
       ),
+      onChanged: (value) => model?.groupName = value,
+      onEditingComplete: () => model?.saveGroup(context),
     );
   }
 }
--- lib/widgets/group_form/group_form_widget_model.dart
+++ lib/widgets/group_form/group_form_widget_model.dart
@@ -1,5 +1,35 @@
 import 'package:flutter/material.dart';
 
-class GroupFormWidgetModel extends ChangeNotifier {
-  
+class GroupFormWidgetModel {
+  var groupName='';
+  void saveGroup(BuildContext context){
+    print(groupName);
+  }
+}
+
+class GroupFormWidgetModelProvider extends InheritedWidget {
+  final GroupFormWidgetModel model;
+
+  const GroupFormWidgetModelProvider({
+    super.key,
+    required super.child,
+    required this.model,
+  });
+
+  static GroupFormWidgetModelProvider? watch(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<GroupFormWidgetModelProvider>();
+  }
+
+  static GroupFormWidgetModelProvider? read(BuildContext context) {
+    final widget = context
+        .getElementForInheritedWidgetOfExactType<GroupFormWidgetModelProvider>()
+        ?.widget;
+    return widget is GroupFormWidgetModelProvider ? widget : null;
+  }
+
+  @override
+  bool updateShouldNotify(GroupFormWidgetModelProvider oldWidget) {
+    return true;
+  }
 }
