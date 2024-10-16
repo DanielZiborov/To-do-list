260be9c702785e2191f971d9c84abf3cd81604b8
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-16 14:56:09 +0300

    переход с экрана tasks на tasks form

--- lib/widgets/tasks/tasks_widget.dart
+++ lib/widgets/tasks/tasks_widget.dart
@@ -43,7 +43,7 @@ class TasksWidgetBody extends StatelessWidget {
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => model?.showForm(context),
         child: const Icon(Icons.add),
       ),
       body: Container(),
